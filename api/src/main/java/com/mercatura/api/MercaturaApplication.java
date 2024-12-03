2a85887bde1f031127c29696c3caa6ac57bd3713
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2024-12-03 15:02:35 +0100

    initialized spring boot project

---
+++ api/src/main/java/com/mercatura/api/MercaturaApplication.java
@@ -0,0 +1,13 @@
+package com.mercatura.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaturaApplication.class, args);
+	}
+
+}
